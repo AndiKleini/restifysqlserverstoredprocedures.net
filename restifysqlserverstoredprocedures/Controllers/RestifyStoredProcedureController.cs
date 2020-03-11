b74dd8beb43013eb9df923b8898e929503440d20
Andreas Kleinbichler <andreas.kleinbichler@admiral.at>
2020-03-11 18:57:29 +0100

    Removed GenerateExecuteStatement because it was not needed anylonger.

--- restifysqlserverstoredprocedures/Controllers/RestifyStoredProcedureController.cs
+++ restifysqlserverstoredprocedures/Controllers/RestifyStoredProcedureController.cs
@@ -10,15 +10,6 @@ namespace restifysqlserverstoredprocedures.Controllers
     [Route("restifysp")]
     public class RestifyStoredProcedureController : ControllerBase
     {
-        [HttpGet("{shema}/{executeSpStatement}")]
-        public async Task<string> Get([FromRoute] string shema, [FromRoute] string executeSpStatement)
-        {
-            Console.Write(shema);
-            var db = new DatabaseAccess("server=ADM000126469;Database=restifysqlserver;Trusted_Connection=Yes;");
-            return JsonConvert.SerializeObject(
-                await db.Execute(GenerateExecuteStatement.FromSubroute(shema, executeSpStatement)));
-        }
-
         [HttpGet("v2/{shema}/{procedurename}/{arguments}")]
         public async Task<string> ExecuteParameterSyntax([FromRoute] string shema, [FromRoute] string procedurename, [FromRoute] string arguments)
         {
