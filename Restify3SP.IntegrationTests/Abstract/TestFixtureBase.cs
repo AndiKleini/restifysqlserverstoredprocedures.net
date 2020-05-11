2f47e88c8fa44b029409351e6addddc2c454f159
DEV\aklein <Andreas.Kleinbichler@admiral.at>
2020-05-11 19:13:58 +0200

    Add first integration test for SingleResultSet behaviour.

---
+++ Restify3SP.IntegrationTests/Abstract/TestFixtureBase.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Restify3SP.IntegrationTests.Engine;
+using System.Threading.Tasks;
+
+namespace Restify3SP.IntegrationTests.Abstract
+{
+    public class TestFixtureBase
+    {
+        protected ScriptRunner Runner {
+            get 
+            {
+                return ScriptRunnerRepository.GetInstance();
+            }
+            private set { }
+        }
+
+        protected async Task<string> Act(string procedureName, string parameter)
+        {
+            return JsonConvert.SerializeObject(
+                await DataBaseAccessRepository.GetInstance().ExecuteWithParameter(
+                    procedureName, 
+                    parameter));
+        }
+    }
+}
---
+++ Restify3SP.IntegrationTests/Engine/ScriptRunnerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace Restify3SP.IntegrationTests.Engine
+{
+    internal static class ScriptRunnerRepository
+    {
+        private static ScriptRunner instance;
+        public static ScriptRunner GetInstance()
+        {
+            return instance;
+        }
+        public static void SetInstance(ScriptRunner runner)
+        {
+            instance = runner;
+        }
+    }
+}
