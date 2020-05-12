a6556ef796dca750bffb6b7bdf3cc8af137aaf66
DEV\aklein <Andreas.Kleinbichler@admiral.at>
2020-05-12 07:45:44 +0200

    Add additional test cases to integration test.

---
+++ Restify3SP.IntegrationTests/TestNoResultSetAtAll.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Restify3SP.IntegrationTests.Abstract;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Restify3SP.IntegrationTests
+{
+    [TestFixture]
+    public class TestNoResultSetAtAll : TestFixtureBase
+    {
+        [SetUp]
+        public async Task Setup()
+        {
+            try
+            {
+                await this.Runner.ExecuteScriptFromFile("./DB/TB_datStudents.txt");
+                await this.Runner.ExecuteScriptFromFile("./DB/Fill_TB_datStudents.txt");
+                await this.Runner.ExecuteScriptFromFile("./DB/SP_UpdateOnlyWithNoResult.txt");
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+            }
+        }
+
+        [Test]
+        public async Task ExecuteSP_EmitsSingleResultSet_ReturnsProperJson()
+        {
+            var result = await this.Act("[restify].[UpdateOnlyWithNoResult]", null);
+
+            result.Should().Be("{\"Result\":[],\"OutputParameter\":{},\"Return\":0}");
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await this.Runner.ExecuteScriptFromFile("./DB/Drop_SP_UpdateOnlyWithNoResult.txt");
+            await this.Runner.ExecuteScriptFromFile("./DB/Drop_TB_datStudents.txt");
+        }
+    }
+}
