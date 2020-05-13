9eded5db6639fdfcd9613fa12d05dd13751e845b
Andreas Kleinbichler <andreas.kleinbichler@admiral.at>
2020-05-13 13:22:03 +0200

    Commit changes regarding connection strings in configuration.

--- restifysqlserverstoredprocedures/Controllers/RestifyStoredProcedureController.cs
+++ restifysqlserverstoredprocedures/Controllers/RestifyStoredProcedureController.cs
@@ -14,7 +14,7 @@ namespace restifysqlserverstoredprocedures.Controllers
         public async Task<string> ExecuteParameterSyntax([FromRoute] string shema, [FromRoute] string procedurename, [FromRoute] string arguments)
         {
             
-            var db = new DatabaseAccess("server=aag-cs.dev03.dev.admiral.local,46005;Database=Admiral_CentralSystem01;Trusted_Connection=Yes;");
+            var db = new DatabaseAccess("server=ADM000126469;Database=restifysqlserver;Trusted_Connection=Yes;");
             // split argument part in procedurename and parameters
             return JsonConvert.SerializeObject(
                 await db.ExecuteWithParameter(shema + "." + procedurename, arguments));
